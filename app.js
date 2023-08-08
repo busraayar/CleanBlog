52e112635e7dc04ead458a229dbd668d1cd69dbd
busraayar <busrayar127@gmail.com>
2023-08-08 23:57:47 +0300

    committed

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
-//get request içerisinde const blog = { id: 1, title: "Blog title", description: "Blog description" }, içeriğini gönderelim.
+const express = require('express');
+const path = require('path');
 
-const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  const blog = {
-    id: 1,
-    title: "Busra'nın Sayfası",
-    description: "Ben Busra. Bu benim blogum ve blogumda gunluk hayatimi paylasacagim",
-  };
-  res.send(blog);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
 });
 
 const port = 3000;
